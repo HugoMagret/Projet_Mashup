07f8806568fa8418c4d8e6cd3ea6b05ec96f1dd8
Ronan <etud@localhost>
2025-11-25 10:16:30 +0100

    mise en place d'une factory pour les clients

--- virtualCRM/src/main/java/org/example/client/SalesforceClient.java
+++ virtualCRM/src/main/java/org/example/client/SalesforceClient.java
@@ -6,15 +6,16 @@ import org.springframework.stereotype.Component;
 import java.util.Collections;
 import java.util.List;
 
-@Component
 public class SalesforceClient {
+
+    SalesforceClient() {
+    }
 
     public List<VirtualLeadDTO> findLeads(double minRevenue, double maxRevenue, String province) {
         // 1) si pas de token ou expiré -> authentification REST Salesforce
         // 2) construire une requête SOQL avec les filtres
         // 3) appeler /services/data/.../query?q=...
         // 4) mapper la réponse JSON -> VirtualLeadDTO (ou DTO intermédiaire)
-
 
         return Collections.emptyList();
     }
--- virtualCRM/src/main/java/org/example/config/ClientConfig.java
+++ virtualCRM/src/main/java/org/example/config/ClientConfig.java
@@ -1,15 +1,34 @@
 package org.example.config;
 
+import org.example.client.ClientFactory;
+import org.example.client.GeoClient;
 import org.example.client.InternalCRMClient;
+import org.example.client.SalesforceClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ClientConfig {
 
+    private final ClientFactory clientFactory;
+
+    public ClientConfig(ClientFactory clientFactory) {
+        this.clientFactory = clientFactory;
+    }
+
     @Bean
     public InternalCRMClient internalCRMClient() throws Exception {
-        return new InternalCRMClient("localhost", 9090);
+        return clientFactory.createInternalCRMClient("localhost", 9090);
+    }
+
+    @Bean
+    public SalesforceClient salesforceClient() {
+        return clientFactory.createSalesforceClient();
+    }
+
+    @Bean
+    public GeoClient geoClient() {
+        return clientFactory.createGeoClient();
     }
 
 }
